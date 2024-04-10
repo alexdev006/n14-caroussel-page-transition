fda40c4fb1c56966248006bef8687088481c518b
Alex <alexdev0605@gmail.com>
2024-04-10 11:09:20 +0200

    feat: style navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,34 @@
-import Link from "next/link";
+'use client';
+
+import { NavLink } from '@/types/navLinks.js';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation.js';
+
+const navLinks: NavLink[] = [
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/about' },
+  { name: 'Contact', href: '/contact' },
+] as const;
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <nav className=" border-b-2 border-slate-950 p-4 font-bold tracking-tight">
+    <nav className=" border-b-2 border-slate-950 p-4 tracking-tight">
       <div className="flex gap-3">
-        <Link href="/">Home</Link>
-        <Link href="about">About</Link>
-        <Link href="contact">Contact</Link>
+        {navLinks.map(({ name, href }) => (
+          <Link
+            key={name}
+            href={href}
+            className={`${
+              pathname === href
+                ? 'underline-offset-8 underline decoration-sky-500 font-bold decoration-2'
+                : ''
+            }`}
+          >
+            {name}
+          </Link>
+        ))}
       </div>
     </nav>
   );
